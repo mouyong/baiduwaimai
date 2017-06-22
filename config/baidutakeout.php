924d4297eaca7160fa326820dc37b5ad8d61561a
牟勇 <my24251325@gmail.com>
2017-06-22 12:08:46 +0800

    将本地一些文件配置移动至 config 目录中的 baidutakeout 文件中

---
+++ config/baidutakeout.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'yilianyun_print_api_url' => env('YILIANYUN_PRINT_API_URL', 'http://open.10ss.net:8888'),
+
+    'baidu_take_out_api_url' => env('BAIDU_TAKE_OUT_API_URL', 'https://api.waimai.baidu.com'),
+    'baidu_shop_info_url' => env('BAIDU_SHOP_INFO_URL', 'http://yilianyun.10ss.net/bdwm/bdwminfo'),
+];
