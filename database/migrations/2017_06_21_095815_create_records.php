843737b0b5851a3a5478922c52b2208273299d0b
牟勇 <my24251325@gmail.com>
2017-06-25 23:59:13 +0800

    将本地的一些东西修改为线上，修改数据库表结构

--- database/migrations/2017_06_21_095815_create_records.php
+++ database/migrations/2017_06_21_095815_create_records.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Records extends Migration
+class CreateRecords extends Migration
 {
     /**
      * Run the migrations.
@@ -19,8 +19,7 @@ class Records extends Migration
             $table->string('order_id', 36)->comment('百度订单 id');
             $table->longText('content')->comment('商户打印的内容');
 
-            $table->string('baidu_source', 20);
-            $table->string('baidu_secret_key', 60);
+            $table->string('baidu_shop_id', 20);
             $table->integer('yilianyun_user_id')->comment('易联云用户 id');
             $table->string('yilianyun_api_key', 40)->comment('易联云用户 api_key');
             $table->text('machines')->comment('易联云用户在用户确认订单时所拥有的打印终端');
