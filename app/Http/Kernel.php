80bf0dd3f5d87d9d877dfb9c016a51c99e1114f1
牟勇 <my24251325@gmail.com>
2017-07-17 01:30:25 +0800

    删除 forward 中间件

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,7 +56,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'forward' => \App\Http\Middleware\ApiForward::class,
         'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 }
